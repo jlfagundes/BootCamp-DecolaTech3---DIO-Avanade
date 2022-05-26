0d2052a6c55be2fc1fed67adc1ddd1e3c8fa6a9f
jlfagundes <jlfagundes@gmail.com>
2022-05-26 15:10:08 -0300

    feat: insert get and set cartaoCredito

--- POO_javascript/POO.js
+++ POO_javascript/POO.js
@@ -1,7 +1,8 @@
 // step 1 - criar classe ContaBancaria começa com letra maiúscula
-// step 2 - dentro da classe ContaBancaria construa o getter e o setter do saldo
+// step 2 - dentro da classe ContaBancaria construa o get e o set do saldo
 // step 3 - criar os metodos sacar e depositar
 // step 4 - criar classe filha ContaCorrente que vai ter cartão de crédito
+// step 5 - criar o get e o set do cartaoCredito
 
 class ContaBancaria {
   constructor(agencia, numero, tipo, saldo) {
@@ -39,6 +40,14 @@ class ContaCorrente {
   constructor(agencia, numero, saldo, cartaoCredito) {
     super(agencia, numero, saldo); // mandar o que receber para classe pai que é ContaBancaria
     this.tipo = 'corrente'; // já seta o tipo de conta
-    this.cartaoCredito = cartaoCredito
+    this._cartaoCredito = cartaoCredito
+  }
+
+  get cartaoCredito() {
+    return this._cartaoCredito
+  }
+
+  set cartaoCredito(valor) {
+    this._cartaoCredito = valor;
   }
 }

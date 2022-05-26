f3d6a0d069a60e317e03935b1ba7a62b6d08d976
jlfagundes <jlfagundes@gmail.com>
2022-05-26 14:38:41 -0300

    feat: step 2 - insert get and set in class

--- POO_javascript/POO.js
+++ POO_javascript/POO.js
@@ -1,10 +1,19 @@
 // step 1 - criar classe ContaBancaria começa com letra maiúscula
+// step 2 - dentro da classe ContaBancaria construa o getter e o setter do saldo
 
 class ContaBancaria {
   constructor(agencia, numero, tipo, saldo) {
     this.agencia = agencia;
     this.numero = numero;
     this.tipo = tipo;
-    this.saldo = saldo;
+    this._saldo = saldo; // quando vai usar no getter ou setter colo o "_" antes para não confundir com propriedade
+  }
+
+  get saldo() {
+    return this._saldo;
+  }
+
+  set saldo(valor) {
+    this._saldo = valor;
   }
 }

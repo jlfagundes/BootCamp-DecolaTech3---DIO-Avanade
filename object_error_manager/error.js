b0e8a9815c40c628f62859a1395945aec8e24b23
jlfagundes <jlfagundes@gmail.com>
2022-05-26 09:56:33 -0300

    feat: step 4 - verify num type number

--- object_error_manager/error.js
+++ object_error_manager/error.js
@@ -1,10 +1,15 @@
 // 1 - criar fjunção que recebe array e numero
 // 2 - validações dos parametros se existem
 // 3 - validar se array é do tipo object
+// 4 - validar se num é do tipo number
 function validaArray(arr, num) {
-  if (!arr && !num) throw new ReferenceError("Enviar os parâmetros");
+  if (!arr && !num) throw new ReferenceError('Enviar os parâmetros');
 
-  if(typeof arr !== 'object') throw new TypeError("Array precisa ser do tipo object ");
+  if(typeof arr !== 'object') throw new TypeError('Array precisa ser do tipo object');
+
+  if (typeof num !== 'number') throw new TypeError('Numero precisa ser do tipo number');
+
+  
 
   return;
 }

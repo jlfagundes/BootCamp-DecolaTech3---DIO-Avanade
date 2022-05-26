9c709a0e8df1a5bb9ab14bb138c60fd9743d44dc
jlfagundes <jlfagundes@gmail.com>
2022-05-26 09:51:53 -0300

    feat: step 3 - verify type array

--- object_error_manager/error.js
+++ object_error_manager/error.js
@@ -1,6 +1,10 @@
 // 1 - criar fjunção que recebe array e numero
 // 2 - validações dos parametros se existem
+// 3 - validar se array é do tipo object
 function validaArray(arr, num) {
-  if (!arr && !num) throw ReferenceError("Enviar os parâmetros");
+  if (!arr && !num) throw new ReferenceError("Enviar os parâmetros");
+
+  if(typeof arr !== 'object') throw new TypeError("Array precisa ser do tipo object ");
+
   return;
 }

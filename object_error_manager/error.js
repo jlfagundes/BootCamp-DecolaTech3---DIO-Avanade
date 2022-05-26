64e925f17ce0704369846756900d01ccc687f43e
jlfagundes <jlfagundes@gmail.com>
2022-05-26 09:48:47 -0300

    feat: step 2 - validate params function

--- object_error_manager/error.js
+++ object_error_manager/error.js
@@ -1,4 +1,6 @@
 // 1 - criar fjunção que recebe array e numero
+// 2 - validações dos parametros se existem
 function validaArray(arr, num) {
+  if (!arr && !num) throw ReferenceError("Enviar os parâmetros");
   return;
 }

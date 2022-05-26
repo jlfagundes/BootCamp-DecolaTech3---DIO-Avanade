c6abd3ef0a0afc94a05216d91f07a6b2af697ed8
jlfagundes <jlfagundes@gmail.com>
2022-05-26 14:33:03 -0300

    feat: create structure class ContaBancaria

---
+++ POO_javascript/POO.js
@@ -0,0 +1,10 @@
+// step 1 - criar classe ContaBancaria começa com letra maiúscula
+
+class ContaBancaria {
+  constructor(agencia, numero, tipo, saldo) {
+    this.agencia = agencia;
+    this.numero = numero;
+    this.tipo = tipo;
+    this.saldo = saldo;
+  }
+}

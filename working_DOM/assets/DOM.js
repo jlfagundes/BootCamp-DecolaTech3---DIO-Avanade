21d4e75ae2bef081c5ccfcb7c532206f732c2f4f
jlfagundes <jlfagundes@gmail.com>
2022-05-27 09:59:40 -0300

    feat: step 2 - get other elements HTML

--- working_DOM/assets/DOM.js
+++ working_DOM/assets/DOM.js
@@ -1,10 +1,16 @@
 // step 1 - capturar botao do html e acionar evento de click chamando função changeMode()
+// step 2 - capturar dos elementos HTML
 
 function changeMode() {
-  console.log(btnSelector)
+  console.log(body)
   return;
 }
 
 const btnSelector = document.getElementById('selector');
+const h1 = document.getElementById('title');
+const body = document.getElementsByTagName('body')[0]; // retorna um array
+const header = document.getElementsByTagName('header')[0];
+const footer = document.getElementsByTagName('footer')[0];
+
 
 btnSelector.addEventListener('click', changeMode);

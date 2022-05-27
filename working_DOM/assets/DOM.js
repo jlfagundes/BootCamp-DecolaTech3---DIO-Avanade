1e062927579b606b2f0c4ea0b42808fd89fa29b5
jlfagundes <jlfagundes@gmail.com>
2022-05-27 10:06:47 -0300

    feat: function change class dark-mode

--- working_DOM/assets/DOM.js
+++ working_DOM/assets/DOM.js
@@ -1,9 +1,17 @@
 // step 1 - capturar botao do html e acionar evento de click chamando função changeMode()
 // step 2 - capturar dos elementos HTML
+// step 3 - mudar a classe
 
 function changeMode() {
-  console.log(body)
-  return;
+  changeClass()
+}
+
+// criando função com função especifica - Princípio da Responsabilidade Única - SOLID
+function changeClass() {
+  btnSelector.classList.toggle('dark-mode');
+  h1.classList.toggle('dark-mode');
+  header.classList.toggle('dark-mode');
+  body.classList.toggle('dark-mode');
 }
 
 const btnSelector = document.getElementById('selector');

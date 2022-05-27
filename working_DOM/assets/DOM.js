a00aa2194ee814daff593b32b1c2246636a09699
jlfagundes <jlfagundes@gmail.com>
2022-05-27 10:16:20 -0300

    feat: step 4 - create dark-mode style

--- working_DOM/assets/DOM.js
+++ working_DOM/assets/DOM.js
@@ -1,6 +1,7 @@
 // step 1 - capturar botao do html e acionar evento de click chamando função changeMode()
 // step 2 - capturar dos elementos HTML
 // step 3 - mudar a classe
+// step 4 - criar class dark-mode
 
 function changeMode() {
   changeClass()
@@ -10,14 +11,13 @@ function changeMode() {
 function changeClass() {
   btnSelector.classList.toggle('dark-mode');
   h1.classList.toggle('dark-mode');
-  header.classList.toggle('dark-mode');
   body.classList.toggle('dark-mode');
+  footer.classList.toggle('dark-mode');
 }
 
 const btnSelector = document.getElementById('selector');
 const h1 = document.getElementById('title');
 const body = document.getElementsByTagName('body')[0]; // retorna um array
-const header = document.getElementsByTagName('header')[0];
 const footer = document.getElementsByTagName('footer')[0];
 
 
